98f212dc8ffa592a4d1126c7ffe6b9d8a7f9b1e5
vidurkhanal <vkhanal@caldwell.edu>
2024-05-25 00:17:43 -0400

    chore: added read_all and write_all for connection fd

---
+++ src/ioutils.cpp
@@ -0,0 +1,32 @@
+#include "ioutils.h"
+#include <cassert>
+#include <cstdio>
+#include <unistd.h>
+
+void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+int32_t write_all(int fd, const char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
---
+++ src/ioutils.h
@@ -0,0 +1,23 @@
+#ifndef IO_UTILS_H
+#define IO_UTILS_H
+
+#include <cassert>
+#include <cstdio>
+#include <unistd.h>
+
+void msg(const char *msg);
+
+/*
+The read() syscall just returns whatever data is available in the kernel, or
+blocks if there is none. It’s up to the application to handle insufficient data.
+The read_full() function reads from the kernel until it gets exactly n bytes.
+*/
+int32_t read_full(int fd, char *buf, size_t n);
+
+/*
+The write() syscall may return successfully with partial data written if the
+kernel buffer is full, we must keep trying when the write() returns fewer bytes
+than we need.
+*/
+int32_t write_all(int fd, const char *buf, size_t n);
+#endif // !DEBUG
