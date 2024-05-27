d21c9301ebf4d61612d6296752825df36f784f1f
vidurkhanal <vkhanal@caldwell.edu>
2024-05-26 21:54:19 -0400

    feat: Abstracted Conn for event loop

---
+++ src/conn.cpp
@@ -0,0 +1,56 @@
+#include "conn.h"
+#include "ioutils.h"
+#include <fcntl.h>
+
+namespace Conn {
+void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn) {
+  if (fd2conn.size() <= (size_t)conn->fd) {
+    fd2conn.resize(conn->fd + 1);
+  }
+  fd2conn[conn->fd] = conn;
+}
+
+static void fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    Server::die("fcntl error");
+    return;
+  }
+
+  flags |= O_NONBLOCK;
+
+  errno = 0;
+  (void)fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    Server::die("fcntl error");
+  }
+}
+
+int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
+  // accept
+  struct sockaddr_in client_addr = {};
+  socklen_t socklen = sizeof(client_addr);
+  int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+  if (connfd < 0) {
+    IOUtils::msg("accept() error");
+    return -1; // error
+  }
+
+  // set the new connection fd to nonblocking mode
+  fd_set_nb(connfd);
+  // creating the struct Conn
+  struct Conn *conn = (struct Conn *)malloc(sizeof(struct Conn));
+  if (!conn) {
+    close(connfd);
+    return -1;
+  }
+  conn->fd = connfd;
+  conn->state = STATE_REQ;
+  conn->rbuf_size = 0;
+  conn->wbuf_size = 0;
+  conn->wbuf_sent = 0;
+  conn_put(fd2conn, conn);
+  return 0;
+}
+} // namespace Conn
---
+++ src/conn.h
@@ -0,0 +1,31 @@
+#ifndef CONN_H
+#define CONN_H
+
+#include "server.h"
+#include <cstddef>
+#include <cstdint>
+
+namespace Conn {
+enum {
+  STATE_REQ = 0,
+  STATE_RES = 1,
+  STATE_END = 2, // mark the connection for deletion
+};
+
+struct Conn {
+  int fd = -1;
+  uint32_t state = 0; // either STATE_REQ or STATE_RES
+  // buffer for reading
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + Server::MAX_MESSAGE_SIZE];
+  // buffer for writing
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + Server::MAX_MESSAGE_SIZE];
+};
+
+void conn_put(std::vector<Conn *> &fd2conn, struct Conn *conn);
+int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd);
+} // namespace Conn
+
+#endif // CONN_H
