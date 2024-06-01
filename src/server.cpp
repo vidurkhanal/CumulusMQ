744b50dd2e89d7869d0a01eec990c3622fa2fa72
vidurkhanal <vkhanal@caldwell.edu>
2024-05-31 22:26:24 -0400

    chore: cleanup after moving to event loop

--- src/server.cpp
+++ src/server.cpp
@@ -91,7 +91,7 @@ static void state_res(Conn::Conn *conn) {
   }
 }
 
-static bool try_one_request(Conn::Conn *conn) {
+static bool handle_conn(Conn::Conn *conn) {
   // try to parse a request from the buffer
   if (conn->rbuf_size < 4) {
     // not enough data in the buffer. Will retry in the next iteration
@@ -147,9 +147,6 @@ static bool try_one_request(Conn::Conn *conn) {
   topic_buf[body_length] = '\0';
   printf("Body: %s\n", body_buf);
   ptr += body_length;
-
-  // got one request, do something with it
-  /* printf("client says: %.*s\n", len, &conn->rbuf[4]); */
 
   // generating echoing response
   memcpy(&conn->wbuf[0], &len, 4);
@@ -208,7 +205,7 @@ static bool try_fill_buffer(Conn::Conn *conn) {
 
   // Try to process requests one by one.
   // Why is there a loop? Please read the explanation of "pipelining".
-  while (try_one_request(conn)) {
+  while (handle_conn(conn)) {
   }
   return (conn->state == Conn::STATE_REQ);
 }
@@ -289,22 +286,6 @@ void Server::Start() {
     if (poll_args[0].revents) {
       (void)Conn::accept_new_conn(fd2conn, fd_);
     }
-
-    /* // accept */
-    /* struct sockaddr_in client_addr = {}; */
-    /* socklen_t socklen = sizeof(client_addr); */
-    /* int connfd = accept(fd_, (struct sockaddr *)&client_addr, &socklen); */
-    /* if (connfd < 0) { */
-    /*   continue; // error */
-    /* } */
-    /**/
-    /* while (true) { */
-    /*   int32_t err = handle_connection(connfd); */
-    /*   if (err) { */
-    /*     break; */
-    /*   } */
-    /* } */
-    /* close(connfd); */
   }
 }
 
@@ -314,122 +295,4 @@ void die(const char *msg) {
   abort();
 }
 
-int32_t Server::handle_connection(int connfd) {
-
-  /* | Message Length | Message Type | Body Length | Message Body |   */
-  /* |----------------|--------------|-------------|--------------| */
-  /* | 4 bytes        | 1 byte       | 4 bytes     | n bytes(Max of 4MB)      |
-   */
-
-  /* | Message Type Value | Action      | */
-  /* |--------------------|-------------| */
-  /* | 0x01               | Subscribe   | */
-  /* | 0x02               | Unsubscribe | */
-  /* | 0x03               | Publish     | */
-  /* | 0x04               | Retrieve    | */
-
-  char rbuf[MAX_MESSAGE_SIZE];
-  errno = 0;
-
-  int ptr = 0;
-
-  // Read the Message Length
-  int32_t err = IOUtils::read_exact(connfd, &rbuf[ptr], 4);
-  if (err) {
-    if (errno == 0) {
-      IOUtils::msg("EOF");
-    } else {
-      IOUtils::msg("read() error");
-    }
-    return err;
-  }
-
-  uint32_t message_length = 0;
-  memcpy(&message_length, rbuf, 4);
-  if (message_length > MAX_MESSAGE_SIZE) {
-    IOUtils::msg("too long");
-    return -1;
-  }
-  printf("Message Length: %u\n", message_length);
-
-  // Read and Process the Message Type byte
-  ptr += 4;
-  err = IOUtils::read_exact(connfd, &rbuf[ptr], 1);
-  if (err) {
-    IOUtils::msg("read() error");
-    return err;
-  }
-
-  uint8_t message_type = 0;
-  memcpy(&message_type, &rbuf[ptr], 1);
-
-  Actions action = get_action(message_type);
-  if (action == Actions::Unknown) {
-    IOUtils::msg("unknown action");
-    return -1;
-  }
-
-  printf("Action: %u\n", action);
-
-  // Read the Topic Length
-  ptr += 1;
-  err = IOUtils::read_exact(connfd, &rbuf[ptr], 4);
-  if (err) {
-    IOUtils::msg("read() error");
-    return err;
-  }
-
-  uint32_t topic_length = 0;
-  memcpy(&topic_length, &rbuf[ptr], 4);
-  printf("Topic Length: %u\n", topic_length);
-
-  // read the topic
-  ptr += 4;
-  err = IOUtils::read_exact(connfd, &rbuf[ptr], topic_length);
-  if (err) {
-    IOUtils::msg("read()");
-    return err;
-  }
-
-  std::string topic(&rbuf[ptr], topic_length);
-  printf("Topic: %s\n", topic.c_str());
-
-  // Read the Body Length
-  ptr += topic_length;
-  err = IOUtils::read_exact(connfd, &rbuf[ptr], 4);
-  if (err) {
-    IOUtils::msg("read() error");
-    return err;
-  }
-
-  uint32_t body_length = 0;
-  memcpy(&body_length, &rbuf[ptr], 4);
-  if (message_length > 4096) {
-    IOUtils::msg("too long");
-    return -1;
-  }
-  printf("Body Length: %u\n", body_length);
-
-  // read the body
-  ptr += 4;
-  err = IOUtils::read_exact(connfd, &rbuf[ptr], body_length);
-  if (err) {
-    IOUtils::msg("read()");
-    return err;
-  }
-
-  std::string body(&rbuf[ptr], body_length);
-  printf("Body: %s\n", body.c_str());
-
-  ptr += body_length;
-  rbuf[ptr] = '\0';
-
-  // reply using the same protocol
-  const char reply[] = "world";
-  char wbuf[4 + sizeof(reply)];
-  message_length = (uint32_t)strlen(reply);
-  memcpy(wbuf, &message_length, 4);
-  memcpy(&wbuf[4], reply, message_length);
-  return IOUtils::write_all(connfd, wbuf, 4 + message_length);
-}
 } // namespace Server
