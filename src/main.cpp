a72c082c0ed80451e302c7d5ccab456ae4c6e349
vidurkhanal <vkhanal@caldwell.edu>
2024-06-10 13:41:38 -0400

    feat: graceful shutdown handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include "server.h"
 #include "storage.h"
-#include <cstddef>
+#include <iostream>
 
+Server::Server *g_server = nullptr; // Global pointer to the Server instance
+
+// Signal handler function
+void signalHandler(int signal) {
+  if (signal == SIGINT || signal == SIGTERM) {
+    if (g_server != nullptr) {
+      std::cout << "\n"
+                << "Gracefully shutting down the server..." << "\n";
+      delete g_server;
+      g_server = nullptr;
+    }
+    exit(0); // Exit the program
+  }
+}
 int main() {
   Server::ServerConfig config = {
       8080,
@@ -10,6 +24,15 @@ int main() {
   StorageFactory storage_factory = StorageFactory();
   StorageType storage_type = StorageType::MemoryStorage;
 
-  Server::Server server(config, storage_factory, storage_type);
-  server.start();
+  signal(SIGINT, signalHandler);
+  signal(SIGTERM, signalHandler);
+
+  g_server = new Server::Server(config, storage_factory, storage_type);
+  g_server->start();
+
+  while (true) {
+    pause(); // Pause the main thread until a signal is received
+  }
+
+  return 0;
 }
